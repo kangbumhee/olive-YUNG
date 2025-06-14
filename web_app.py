dcfa6cce0789f13263df7b2b34c109e47889d509
kangbumhee <kbhjjan@naver.com>
2025-06-14 23:58:02 +0900

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -220,4 +220,6 @@ def export_excel():
 if __name__ == '__main__':
     ensure_chromium_installed()
     load_data()
-    app.run(host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
